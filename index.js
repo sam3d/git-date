66574309887c9f400ffaeb375a035dc590dce37b
Sam Holmes <samholmes1337@gmail.com>
2016-02-27 18:04:19 +0000

    Run command to change commit and author date

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var moment = require("moment");
 var sugar = require("sugar");
 var colors = require("colors");
+var exec = require("child_process").exec;
 
 // Check for CLI argument
 process.argv.splice(0, 2);
@@ -15,7 +16,18 @@ if (process.argv.length > 0) {
 
         // Date could be parsed, parse the date to git date format
         var dateString = moment(parsedDate).format("ddd MMM DD hh:mm:ss YYYY ZZ");
-        console.log("\nModified previous commit:\n\tAUTHOR_DATE " + colors.grey(dateString) + "\n\tCOMMITTER_DATE " + colors.grey(dateString) + "\n");
+
+        // Actually modify the dates
+        var command = "GIT_COMMITTER_DATE=\"" + dateString + "\" git commit --amend --date=\"" + dateString + "\" --no-edit";
+        console.log(command);
+        exec(command, function(err, stdout, stderr){
+            if (err){
+                console.log("fatal: Could not change the previous commit");
+            } else {
+                // Dates modified, notify the user
+                console.log("\nModified previous commit:\n\tAUTHOR_DATE " + colors.grey(dateString) + "\n\tCOMMITTER_DATE " + colors.grey(dateString) + "\n\nCommand executed:\n\t" + colors.bgWhite.black(command) + "\n");
+            }
+        });
 
     } else {
         console.log("fatal: Could not parse \"" + date + "\" into a valid date");
