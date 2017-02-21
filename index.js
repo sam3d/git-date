a1b16dbb6c36ae34e39bbcebfe214985faad88a2
Sam Holmes <samholmes1337@gmail.com>
2017-02-21 11:00:38 +0000

    Small refactor and syntax tidy-up

--- index.js
+++ index.js
@@ -7,36 +7,27 @@ const sugar = require("sugar");
 const chalk = require("chalk");
 const exec = require("child_process").exec;
 
-// Throw a fatal error
 const fatal = err => {
   console.error(`fatal: ${err}`);
-}
+  process.exit(1);
+};
 
 process.argv.splice(0, 2);
 if (process.argv.length === 0) {
   fatal("No date string given");
-} else {
+};
 
-  // Attempt to parse the date
-  let date = process.argv.join(" ");
-  let parsedDate = new sugar.Date.create(date);
+let date = process.argv.join(" ");
+let parsedDate = new sugar.Date.create(date);
 
-  console.log(parsedDate);
-  if (/Invalid Date/.test(parsedDate)) {
-    fatal("Could not parse \"" + date + "\" into a valid date");
-  } else {
+if (/Invalid Date/.test(parsedDate)) {
+  fatal(`Could not parse ${date} into a valid date`);
+};
 
-    // Date could be parsed, parse the date to git date format
-    let dateString = moment(parsedDate).format("ddd MMM DD HH:mm:ss YYYY ZZ");
+let dateString = moment(parsedDate).format("ddd MMM DD HH:mm:ss YYYY ZZ");
 
-    // Actually modify the dates
-    let command = "GIT_COMMITTER_DATE=\"" + dateString + "\" git commit --amend --date=\"" + dateString + "\" --no-edit";
-    exec(command, (err, stdout, stderr) => {
-      if (err) {
-        fatal("Could not change the previous commit");
-      } else {
-        console.log("\nModified previous commit:\n    AUTHOR_DATE " + chalk.grey(dateString) + "\n    COMMITTER_DATE " + chalk.grey(dateString) + "\n\nCommand executed:\n    " + chalk.bgWhite.black(command) + "\n");
-      }
-    });
-  }
-}
+let command = `GIT_COMMITTER_DATE="${dateString}" git commit --amend --date="${dateString}" --no-edit`;
+exec(command, (err, stdout, stderr) => {
+  if (err) fatal("Could not change the previous commit");
+  console.log("\nModified previous commit:\n    AUTHOR_DATE " + chalk.grey(dateString) + "\n    COMMITTER_DATE " + chalk.grey(dateString) + "\n\nCommand executed:\n    " + chalk.bgWhite.black(command) + "\n");
+});
