124905ed4f53e4023bb02706539f5ca851a0b9b3
Sam Holmes <samholmes1337@gmail.com>
2018-02-08 11:15:39 +0000

    Change indentation style to use tabs

--- index.js
+++ index.js
@@ -8,26 +8,26 @@ const chalk = require("chalk");
 const exec = require("child_process").exec;
 
 const fatal = err => {
-  console.error(`fatal: ${err}`);
-  process.exit(1);
+    console.error(`fatal: ${err}`);
+    process.exit(1);
 };
 
 process.argv.splice(0, 2);
 if (process.argv.length === 0) {
-  fatal("No date string given");
+    fatal("No date string given");
 };
 
 let date = process.argv.join(" ");
 let parsedDate = new sugar.Date.create(date);
 
 if (/Invalid Date/.test(parsedDate)) {
-  fatal(`Could not parse ${date} into a valid date`);
+    fatal(`Could not parse ${date} into a valid date`);
 };
 
 let dateString = moment(parsedDate).format("ddd MMM DD HH:mm:ss YYYY ZZ");
 
 let command = `GIT_COMMITTER_DATE="${dateString}" git commit --amend --date="${dateString}" --no-edit`;
 exec(command, (err, stdout, stderr) => {
-  if (err) fatal("Could not modify the date of the previous commit");
-  console.log("\nModified previous commit:\n    AUTHOR_DATE " + chalk.grey(dateString) + "\n    COMMITTER_DATE " + chalk.grey(dateString) + "\n\nCommand executed:\n    " + chalk.bgWhite.black(command) + "\n");
+    if (err) fatal("Could not modify the date of the previous commit");
+    console.log("\nModified previous commit:\n    AUTHOR_DATE " + chalk.grey(dateString) + "\n    COMMITTER_DATE " + chalk.grey(dateString) + "\n\nCommand executed:\n    " + chalk.bgWhite.black(command) + "\n");
 });
