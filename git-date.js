abd00faeb635de1bab91ff5262e5d2068d0c6b79
Sam Holmes <samholmes1337@gmail.com>
2018-11-12 19:18:31 +0000

    Align items and improve coding style

--- git-date.js
+++ git-date.js
@@ -5,9 +5,9 @@
 const moment = require("moment");
 const sugar = require("sugar");
 const chalk = require("chalk");
-const exec = require("child_process").exec;
+const { exec } = require("child_process");
 
-let examples = [
+const examples = [
     "2010",
     "Tuesday May 25th, 2010",
     "the day after tomorrow",
@@ -19,32 +19,32 @@ let examples = [
     "last year"
 ];
 
-let exit = err => {
+function exit(err) {
     if (err) console.log(chalk`{red error: ${err}}`);
 
-    console.log(`usage: git date <date string>\n\nExamples of valid commands:`);
+    console.log("usage: git date <date string>\n\nExamples of valid commands:");
     examples.forEach(example => console.log(`    git date ${example}`));
     console.log();
 
     process.exit(1);
-};
+}
 
 process.argv.splice(0, 2);
-if (process.argv.length === 0) {
+if (!process.argv.length) {
     exit();
-};
+}
 
-let date = process.argv.join(" ");
-let parsedDate = new sugar.Date.create(date);
+const date = process.argv.join(" ");
+const parsedDate = new sugar.Date.create(date);
 
 if (/Invalid Date/.test(parsedDate)) {
     exit(`Could not parse "${date}" into a valid date`);
-};
+}
 
-let dateString = moment(parsedDate).format("ddd MMM DD HH:mm:ss YYYY ZZ");
+const dateString = moment(parsedDate).format("ddd MMM DD HH:mm:ss YYYY ZZ");
 
-let command = `GIT_COMMITTER_DATE="${dateString}" git commit --amend --date="${dateString}" --no-edit`;
+const command = `GIT_COMMITTER_DATE="${dateString}" git commit --amend --date="${dateString}" --no-edit`;
 exec(command, (err, stdout, stderr) => {
     if (err || stderr) exit("Could not modify the date of the previous commit");
-    console.log(chalk`\nModified previous commit:\n    AUTHOR_DATE {grey ${dateString}}\n    COMMITTER_DATE {grey ${dateString}}\n\nCommand executed:\n    {grey ${command}}\n`);
+    console.log(chalk`\nModified previous commit:\n    AUTHOR_DATE    {grey ${dateString}}\n    COMMITTER_DATE {grey ${dateString}}\n\nCommand executed:\n    {grey ${command}}\n`);
 });
